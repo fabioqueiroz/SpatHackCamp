d0e060c412d254de84712f0f40c960e92369a444
r1cards <Bazinga1233>
2020-01-14 15:21:09 +0000

    :)

--- WebApplication/wwwroot/js/customScript.js
+++ WebApplication/wwwroot/js/customScript.js
@@ -32,6 +32,50 @@ function areLoginDetailsValid() {
 var options = [];
 
 function areFieldsValueValid(){
+    var inputName = document.getElementById("firstNameInputUser").value;
+    var inputSurname = document.getElementById("lastNameInputUser").value;
+    var inputGender = document.getElementById("inputGender").value;
+    var inputDofB = document.getElementById("dateOfBirthInputUser").value;
+    var inputMail = document.getElementById("emailInputUser").value;
+    var inputAddress = document.getElementById("inputAddress").value;
+    var inputCity = document.getElementById("inputCity").value;
+    var inputCountry = document.getElementById("inputCountry").value;
+    var inputPostCode = document.getElementById("inputPostCode").value;
     
-    return true;
+    if(inputName.trim() == ''){
+        sendError("Please Enter Your Name!");
+        return false;
+    } else if(inputSurname.trim() == ''){
+        sendError("Please Enter Your Last Name!");
+        return false;
+    } else if(inputGender.trim() == ''){
+        sendError("Please Enter Your Gender!");
+        return false;
+    } else if(inputDofB.trim() == ''){
+        sendError("Please Enter Your Date of Birth!");
+        return false;
+    } else if(inputMail.trim() == ''){
+        sendError("Please Enter Your E-mail!");
+        return false;
+    } else if(inputAddress.trim() == ''){
+        sendError("Please Enter Your Address!");
+        return false;
+    } else if(inputCity.trim() == ''){
+        sendError("Please Enter Your City!");
+        return false;
+    } else if(inputCountry.trim() == ''){
+        sendError("Please Enter Your Country!");
+        return false;
+    } else if(inputPostCode.trim() == ''){
+        sendError("Please Enter Your Post Code!");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function sendError(errorMessage) {
+   var error =  document.getElementById("errorMessage");
+   error.innerText = errorMessage;
+   document.getElementById("errorMessage").style.visibility = "visible";
 }
