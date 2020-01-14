978511fdc75ed21eb737a91f99c4b8b582f219fe
avram <avramandreitiberiu@gmail.com>
2020-01-14 12:52:43 +0000

    -pushing feature - admin is able to create user

---
+++ WebApplication/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult CreateStudent()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitStudentAccount()
+        {
+            return RedirectToAction("");
+        }
+    }
+}
