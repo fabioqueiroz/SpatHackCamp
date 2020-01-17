80f0a6461ff589d8cb6063859cc4c9eb6568bea3
avram <avramandreitiberiu@gmail.com>
2020-01-17 19:58:24 +0000

    another files

---
+++ Rubrics.Data.Access/Migrations/20200117152215_eminem2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rubrics.Data.Access.Migrations
+{
+    public partial class eminem2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GroupId",
+                table: "Students",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupId",
+                table: "Students");
+        }
+    }
+}
