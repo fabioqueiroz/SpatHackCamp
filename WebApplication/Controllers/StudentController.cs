e2a9a3645e28472200d0a2be8fe3371e7b1def52
r1cards <Bazinga1233>
2020-01-21 15:34:08 +0000

    fixed mark showing for student

--- WebApplication/Controllers/StudentController.cs
+++ WebApplication/Controllers/StudentController.cs
@@ -55,7 +55,7 @@ namespace WebApplication.Controllers
             //insert the data into the ViewData object 
             //and send it to the view
             //insert static data for now
-            int[] grades = new[] {30, 80, 76};
+            int[] grades = new[] {3, 5, 7};
             List<RoundModel> pastRounds = new List<RoundModel>();
             pastRounds.Add(new RoundModel() {ModuleName = "Maths", Active = false, Deadline = "12/1/2020"});
             pastRounds.Add(new RoundModel() {ModuleName = "Romanian", Active = false, Deadline = "10/1/2020"});
